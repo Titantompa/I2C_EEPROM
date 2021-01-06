274cabf175add10eb75c8b76a0c8f9fc4a021875
tomas.hubner@hiq.se <tomas.hubner@hiq.se>
2021-01-06 08:57:45 +0100

    Added test for Wire.begin()

---
+++ test/unit_test_002.cpp
@@ -0,0 +1,66 @@
+//
+//    FILE: unit_test_002.cpp
+//  AUTHOR: Tomas Hübner
+//    DATE: 2021-01-06
+// PURPOSE: unit test for the I2C_EEPROM library
+//          https://github.com/RobTillaart/
+//          https://github.com/Arduino-CI/arduino_ci/blob/master/REFERENCE.md
+//
+
+// supported assertions
+// https://github.com/Arduino-CI/arduino_ci/blob/master/cpp/unittest/Assertion.h#L33-L42
+// ----------------------------
+// assertEqual(expected, actual)
+// assertNotEqual(expected, actual)
+// assertLess(expected, actual)
+// assertMore(expected, actual)
+// assertLessOrEqual(expected, actual)
+// assertMoreOrEqual(expected, actual)
+// assertTrue(actual)
+// assertFalse(actual)
+// assertNull(actual)
+// assertNotNull(actual)
+
+#include <ArduinoUnitTests.h>
+
+#define assertEqualFloat(arg1, arg2, arg3)  assertOp("assertEqualFloat", "expected", fabs(arg1 - arg2), compareLessOrEqual, "<=", "actual", arg3)
+#define assertEqualINF(arg)  assertOp("assertEqualINF", "expected", INFINITY, compareEqual, "==", "actual", arg)
+#define assertEqualNAN(arg)  assertOp("assertEqualNAN", "expected", true, compareEqual, "==", "actual", isnan(arg))
+
+#define I2C_EEPROM_WIRE FailsToBuild
+
+#include "Arduino.h"
+#include "I2C_eeprom.h"
+
+unittest_setup()
+{
+}
+
+unittest_teardown()
+{
+}
+
+/*
+unittest(test_new_operator)
+{
+  assertEqualINF(exp(800));
+  assertEqualINF(0.0/0.0);
+  assertEqualINF(42);
+  
+  assertEqualNAN(INFINITY - INFINITY);
+  assertEqualNAN(0.0/0.0);
+  assertEqualNAN(42);
+}
+*/
+
+unittest(wire_begin)
+{
+  I2C_eeprom EE(0x50);
+  EE.begin();
+  
+  assertTrue(Wire.didBegin());
+}
+
+unittest_main()
+
+// --------
